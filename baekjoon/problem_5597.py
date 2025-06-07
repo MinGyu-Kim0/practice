1741e60edab418dc56e3551d0925241b696d04fc
min gyu kim <coinmagic8085@gmail.com>
2025-06-07 20:31:16 +0900

    feat: Solve BOJ 5597

--- baekjoon/problem_5597.py
+++ baekjoon/problem_5597.py
@@ -5,27 +5,10 @@ students = [value for value in range(1, 31)]
 
 # 제출한 28명 학생명단 리스트 생성.
 submit = []
-while True:
-    student = int(input())
-    if student in submit:
-        continue
-    else:
-        submit.append(student)
-    if len(submit) == 28:
-        break
-# 리스트안에 없는 출석번호 체크
-missing = []
-for i in students:
-    if len(missing) == 2:
-        break
-    for j in range(28):
-        if students[i] in submit:
-            break
-        else:
-            missing.append(students[i])
-            break
-# 오름차순으로 출석번호 출력
-print(missing[0])
-print(missing[1])
+for _ in range(28):
+    submit.append(int(input()))
 
+for i in students:
+    if i not in submit:
+        print(i)
     
